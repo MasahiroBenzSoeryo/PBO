4240eed504ff0a9e08e7315a57d5ff1799b039b7
MasahiroBenzSoeryo14628 <112880576+MasahiroBenzSoeryo@users.noreply.github.com>
2024-03-13 21:09:52 +0700

    Add files via upload

---
+++ pertemuan1/latihan1_poinE2a.java
@@ -0,0 +1,17 @@
+package pertemuan1;
+
+public class latihan1_poinE2a {
+
+	public static void main(String[] args) {
+
+		double pi = 3.14;
+		double diameterLingkaran = 10;
+		double jariJariLingkaran = diameterLingkaran / 2;
+		double kelilingLingkaran = 2 * pi * jariJariLingkaran;
+        
+        System.out.println("Diameter lingkaran = 10");
+        System.out.println("Keliling lingkaran dengan diameter 10 adalah = " + kelilingLingkaran);
+
+	}
+
+}
---
+++ pertemuan1/latihan1_poinE2b.java
@@ -0,0 +1,17 @@
+package pertemuan1;
+
+public class latihan1_poinE2b {
+
+	public static void main(String[] args) {
+
+		double alasSegitiga = 6;
+		double tinggiSegitiga = 8;
+		double luasSegitiga = 0.5 * alasSegitiga * tinggiSegitiga;
+        
+        System.out.println("Alas segitiga siku-siku = 6");
+        System.out.println("Tinggi segitiga siku-siku = 8");
+        System.out.println("Luas segitiga siku-siku dengan alas 6 dan tinggi 8 adalah = " + luasSegitiga);
+
+	}
+
+}
