89847c0a426b1ee878f78fa9e81d9923bbbc22fa
MasahiroBenzSoeryo14628 <112880576+MasahiroBenzSoeryo@users.noreply.github.com>
2024-03-13 20:55:01 +0700

    Add files via upload

---
+++ pertemuan1/latihan1_poinE2a.java
@@ -0,0 +1,16 @@
+package pertemuan1;
+
+public class latihan1_poinE2a {
+
+	public static void main(String[] args) {
+
+		double pi = 3.14;
+		double diameterLingkaran = 10;
+        double kelilingLingkaran = pi * diameterLingkaran;
+        
+        System.out.println("Diameter lingkaran = 10");
+        System.out.println("Keliling lingkaran dengan diameter 10 adalah = " + kelilingLingkaran);
+
+	}
+
+}
